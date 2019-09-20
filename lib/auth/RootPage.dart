d87617e418ffa6859bd8c73d6e2f708449a7185b
iamsaikumar <saikumarreddyatluri@gmail.com>
2019-09-20 22:06:49 +0530

    commit

---
+++ lib/auth/RootPage.dart
@@ -0,0 +1,13 @@
+import 'package:a20/views/homescreen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+import './LoginPage.dart';
+
+
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider.of<FirebaseUser>(context)==null?LoginPage():HomeScreen();
+  }
+}
--- lib/views/homescreen.dart
+++ lib/views/homescreen.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomeScreen extends StatelessWidget {
+  HomeScreen({this.clgname});
+  final String clgname;
   @override
   Widget build(BuildContext context) {
+    final double _height= MediaQuery.of(context).size.height;
+    final double _width = MediaQuery.of(context).size.width;
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text('A20~$clgname',style: TextStyle(fontWeight: FontWeight.bold,color:Colors.black.withOpacity(0.65)),),
+        backgroundColor: Colors.white,
+      ),
+      body: Container(
+        height: _height,
+        width: _width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            CarouselSlider(
+              autoPlay: true,
+              height: _height*.35,
+                items: <Widget>[
+                  Container(
+                    child: FlutterLogo(
+                      size: 30,
+                    ),
+                  ),
+                  Container(
+                    child: FlutterLogo(
+                      size: 30,
+                    ),
+                  ),
+                  Container(
+                    child: FlutterLogo(
+                      size: 30,
+                    ),
+                  ),
+                  Container(
+                    child: FlutterLogo(
+                      size: 30,
+                    ),
+                  )
+                ],
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
